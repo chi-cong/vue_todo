c7e51266d1d2069e3dc84ba0ea4840123bd85b78
Chi Cong <chicong422002@gmail.com>
2023-08-06 23:49:57 +0700

    tempory remove i18n setup

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,9 +1,8 @@
 <script lang="ts" setup>
-  
 </script>
 
 <template>
   <header>
-    <h1 class="app-name">{{ $t("message.appName") }}</h1>
+    <h1 class="app-name">{{   }}</h1>
   </header>
 </template>
