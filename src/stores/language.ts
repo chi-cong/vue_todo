00d2f75cea681f9a10e6c941aaa78f4c9cafee3a
Chi Cong <chicong422002@gmail.com>
2023-08-14 23:48:26 +0700

    add language store

---
+++ src/stores/language.ts
@@ -0,0 +1,76 @@
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export type languages = 'vn' | 'en'
+
+interface SetType {
+  appName: string
+  filter: {
+    all: string
+    expired: string
+    undone: string
+  }
+  message: {
+    expirationWarning: string
+  }
+  todoCard: {
+    work: string
+    deadline: string
+  }
+  button: {
+    addCard: string
+    save: string
+    discard: string
+  }
+}
+
+const languageSet = {
+  vn: {
+    appName: 'Danh sách việc',
+    filter: {
+      all: 'Tất cả',
+      expired: 'Hết hạn',
+      undone: 'Cần làm'
+    },
+    message: {
+      expirationWarning: 'Sắp đến hạn'
+    },
+    todoCard: {
+      work: 'Việc: ',
+      deadline: 'Hạn: '
+    },
+    button: {
+      addCard: 'Thêm thẻ mới',
+      save: 'Lưu',
+      discard: 'Hủy'
+    }
+  },
+  en: {
+    appName: 'Todo list',
+    filter: {
+      all: 'All',
+      expired: 'Expired',
+      undone: 'Undone'
+    },
+    message: {
+      expirationWarning: 'Expired soon'
+    },
+    todoCard: {
+      work: 'Work: ',
+      deadline: 'Deadline: '
+    },
+    button: {
+      addCard: 'Add new card',
+      save: 'save',
+      discard: 'discard'
+    }
+  }
+}
+
+export const languageStore = defineStore('language', () => {
+  const mainSet = ref<SetType>(languageSet['vn'])
+  const changeLanguage = (language: languages) => {
+    mainSet.value = languageSet[language]
+  }
+  return { mainSet, changeLanguage }
+})
