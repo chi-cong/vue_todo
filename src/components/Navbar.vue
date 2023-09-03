91813f883a34b157291eff318a69cea7ce692280
Chi Cong <chicong422002@gmail.com>
2023-09-03 19:13:09 +0700

    improve language button UI

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,7 +5,7 @@ const languageStore = useLanguageStore();
 
 <template>
   <header>
-    <h1 class="app-name">{{ languageStore.mainSet.appName}}</h1>
+    <h1 class="app-name">{{ languageStore.mainSet.appName }}</h1>
     <div class="language">
       <button class="language-btn active-language-btn">VN</button>
       <button class="language-btn">EN</button>
@@ -14,36 +14,46 @@ const languageStore = useLanguageStore();
 </template>
 
 <style scoped>
-  header{
-    width: 100%;
-    height: auto;
-    display: flex;
-    padding-top: 15px;
-    padding-bottom: 15px;
-    padding-left: 5%;
-    padding-right: 5%;
-    justify-content: space-between;
-    align-items: center;
-    background: var(--primary);
-    color: var(--white);
-  }
-  .app-name{
-    font-weight: var(--thick-font);
-  }
-  .language{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    gap: 10px;
-  }
-  .language-btn{
-    background: var(--primary);
-    border: var(--normal-white-border);
-    border-radius: var(--rounded-angle);
-    color: var(--white);
-  }
-  .active-language-btn{
-    color: var(--black);
-    background: var(--white);
-  }
+header {
+  width: 100%;
+  height: auto;
+  display: flex;
+  padding-top: 15px;
+  padding-bottom: 15px;
+  padding-left: 5%;
+  padding-right: 5%;
+  justify-content: space-between;
+  align-items: center;
+  background: var(--primary);
+  color: var(--white);
+}
+
+.app-name {
+  font-weight: var(--thick-font);
+}
+
+.language {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+
+.language-btn {
+  background: var(--primary);
+  border: var(--normal-white-border);
+  border-radius: var(--rounded-angle);
+  color: var(--white);
+  transition: background .3s, color .3s;
+}
+
+.active-language-btn {
+  color: var(--black);
+  background: var(--white);
+}
+
+.language-btn:hover {
+  color: var(--black);
+  background: var(--white);
+}
 </style>
